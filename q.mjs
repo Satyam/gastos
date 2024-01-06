f1642d618217d9e6d940b19c9b9b642791766f48
Satyam <satyam@satyam.com.ar>
2024-01-06 18:52:52 +0100

    Me voy acercando a algo usable

--- q.mjs
+++ q.mjs
@@ -1,21 +1,26 @@
 #!/usr/bin/env zx
-import { readAllLines, readConocidos, ymKey } from './utils.mjs';
+import {
+  joinDate,
+  logger,
+  readAllLines,
+  readConocidos,
+  splitDate,
+  ymKey,
+  parseSabadellDate,
+} from './utils.mjs';
+
+let startDate = '9999-99-99',
+  endDate = '0000-00-00';
 
 const readMovimientos = async (fname) => {
-  const parseDate = (ds) =>
-    ds.replace(/(\d+)\/(\d+)\/(\d+)/, (_, d, m, y) =>
-      [y, m.padStart(2, '0'), d.padStart(2, '0')].join('-')
-    );
   const fileContents = await readAllLines(fname);
   return fileContents
     .map((row) => {
       const [d, c, _, i, s] = row.split('|');
-      return [
-        parseDate(d),
-        c.toUpperCase().trim(),
-        parseFloat(i),
-        parseFloat(s),
-      ];
+      const date = parseSabadellDate(d);
+      if (date < startDate) startDate = date;
+      if (date > endDate) endDate = date;
+      return [date, c.toUpperCase().trim(), parseFloat(i), parseFloat(s)];
     })
     .reverse();
 };
@@ -26,9 +31,9 @@ const conocidos = await readConocidos('Conocidos.csv');
 
 const desconocidos = [];
 
-console.log(movimientos), console.log(desconocidos);
+// console.log(movimientos), console.log(desconocidos);
 
-const movHashByYearMonth = () => {
+const movHashByYearMonth = (movimientos) => {
   const hash = {};
   movimientos.forEach((row) => {
     const [date, concepto] = row;
@@ -57,7 +62,8 @@ const movHashByYearMonth = () => {
   });
   return hash;
 };
-const movHashByConcepto = () => {
+
+const movHashByConcepto = (movimientos) => {
   const hash = {};
   movimientos.forEach((row) => {
     const [date, concepto] = row;
@@ -80,6 +86,64 @@ const movHashByConcepto = () => {
   return hash;
 };
 
-console.log(conocidos);
-console.dir(await movHashByConcepto(), { depth: null });
-console.log(desconocidos.sort());
+const hash = movHashByConcepto(movimientos);
+
+// fs.writeFile('salidas/hash.txt', JSON.stringify(hash, null, 2));
+
+const out = logger('salidas/index.html');
+
+out.log(`<html>
+  <head>
+    <style>
+      td,th {
+        border:thin solid silver;
+      }
+      th {
+        font-weight: bolder;
+      }
+    </style>
+  </head>
+  <body>
+  <table>`);
+const [startY, startM] = splitDate(startDate);
+const [endY, endM] = splitDate(endDate);
+
+out.log('<tr><td></td>');
+for (let y = startY, m = startM; y <= endY || m <= endM; m++) {
+  if (m > 12) {
+    m = 1;
+    y++;
+    if (y > endY) break;
+  }
+  out.log('<th>%s</th>', ymKey(joinDate(y, m)));
+}
+out.log('</tr>');
+for (const [concepto, short] of conocidos) {
+  const entriesConcepto = hash[short];
+  out.log('<tr><th>%s</th>', short);
+
+  for (let y = startY, m = startM; y <= endY || m <= endM; m++) {
+    if (m > 12) {
+      m = 1;
+      y++;
+      if (y > endY) break;
+    }
+    const entries = entriesConcepto[ymKey(joinDate(y, m))];
+    if (entries) {
+      out.log('<td><table>');
+      entries.forEach(([d, _, i]) => {
+        out.log('<tr><td>%s</td><td>%s</td></tr>', d, i);
+      });
+      out.log('</table></td>');
+    } else {
+      out.log('<td></td>');
+    }
+  }
+
+  out.log('</tr>');
+}
+
+out.log('</table></body></html>');
+// console.log(conocidos);
+// console.dir(await movHashByConcepto(movimientos), { depth: null });
+// console.log(desconocidos.sort());
--- utils.mjs
+++ utils.mjs
@@ -1,3 +1,4 @@
+import { Console } from 'node:console';
 export const readAllLines = async (fname) => {
   const fileContents = await fs.readFile(fname, 'latin1');
   return fileContents.trim().split('\n');
@@ -14,3 +15,16 @@ export const readConocidos = async (fname) => {
 };
 
 export const ymKey = (date) => date.slice(0, 7);
+
+export const parseSabadellDate = (ds) =>
+  ds.replace(/(\d+)\/(\d+)\/(\d+)/, (_, d, m, y) => joinDate(y, m, d));
+
+export const splitDate = (date) => date.split('-').map((p) => parseInt(p, 10));
+
+export const joinDate = (y, m, d = 1) =>
+  [
+    String(y).padStart(4, '0'),
+    String(m).padStart(2, '0'),
+    String(d).padStart(2, '0'),
+  ].join('-');
+export const logger = (fname) => new Console(fs.createWriteStream(fname));
