781353012b6c373af22873d4cc2592a94b237cdf
Satyam <satyam@satyam.com.ar>
2024-02-09 10:45:34 +0100

    In `Fecha` : added the ability to parse date from and ISO date string, Added a `toDate` method to produce actual `Date` instances. Added `compare` method to use in sorts by date

--- gscript/fecha.gs.js
+++ gscript/fecha.gs.js
@@ -9,6 +9,15 @@ class Fecha {
       this.y = parseInt(y1, 10);
       this.m = parseInt(m1, 10);
       this.d = parseInt(d1, 10);
+    } else if (typeof m === 'undefined' && typeof y === 'string') {
+      const m = Fecha._regexp.exec(y);
+      if (m) {
+        this.y = parseInt(m[1], 10);
+        this.m = parseInt(m[2], 10);
+        this.d = parseInt(m[3], 10);
+      } else {
+        throw new TypeError(`Unable to parse date from string: ${y}`);
+      }
     } else {
       this.y = parseInt(y, 10);
       this.m = parseInt(m, 10);
@@ -16,6 +25,7 @@ class Fecha {
     }
     this._normalize();
   }
+
   _normalize() {
     while (this.m > 12) {
       this.y++;
@@ -41,6 +51,16 @@ class Fecha {
   toString() {
     return `${this.yyyy}-${this.mm}-${this.dd}`;
   }
+  toDate() {
+    return new Date(this.y, this.m - 1, this.d);
+  }
+  /*  valueOf() {
+    return `${this.yyyy}-${this.mm}-${this.dd}`;
+  }
+   [Symbol.toPrimitive](hint) {
+    return `${this.yyyy}-${this.mm}-${this.dd}`;
+  }
+*/
   addMonths(m) {
     this.m += m;
     this._normalize();
@@ -54,7 +74,7 @@ class Fecha {
   nextYear() {
     return new Fecha(this.y + 1, this.m);
   }
-  loopUntil(cb, y, m) {
+  loopUntilMonth(cb, y, m) {
     if (y instanceof Fecha) {
       m = y.m;
       y = y.y;
@@ -67,8 +87,18 @@ class Fecha {
       cb(f);
     }
   }
+  compare(f) {
+    if (f instanceof Date) f = new Fecha(f);
+    let diff = this.y - f.y;
+    if (diff) return diff;
+    diff = this.m - f.m;
+    if (diff) return diff;
+    return this.d - f.d;
+  }
   static fromSabadell(fecha) {
     const [d, m, y] = fecha.split('/');
     return new Fecha(y, m, d);
   }
 }
+
+Fecha._regexp = /(\d{2,4})\D?(\d{1,2})\D?(\d{1,2})/;
