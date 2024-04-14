9e7fa01900d687c740146428d3350d70e6ed723a
Satyam <satyam@satyam.com.ar>
2024-04-14 15:40:56 +0200

    Dropped `sheets.gs` after moving its contents (generic globals) into `Code.gs` along other globals. Added `d` (`for `dedent`) to make template literals less messy.

--- gscript/code.gs.js
+++ gscript/code.gs.js
@@ -2,6 +2,7 @@ const BIG_FONT = 16;
 const BKG_BAND = 'lightgrey';
 const ESTIMATE = 'lightpink';
 const NUMBER_FORMAT = '#,0.00';
+
 // Estos se tienen que corresponder con los de la solapa `Encabezados`
 const HEADINGS = {
   VARIOS: 'Varios',
@@ -13,6 +14,19 @@ const HEADINGS = {
   TARJETA: 'Tarjeta de Crédito',
 };
 
+const sSheet = SpreadsheetApp.getActiveSpreadsheet();
+
+const sh = {
+  historico: sSheet.getSheetByName('Histórico'),
+  conocidos: sSheet.getSheetByName('Conocidos'),
+  totales: sSheet.getSheetByName('Totales'),
+  headings: sSheet.getSheetByName('Encabezados'),
+  desconocidos: sSheet.getSheetByName('Desconocidos'),
+  alquileres: sSheet.getSheetByName('Alquileres'),
+};
+
+const ui = SpreadsheetApp.getUi();
+
 function getUseComma(emptyCell) {
   return emptyCell
     .setNumberFormat('#.#')
@@ -23,4 +37,23 @@ function getUseComma(emptyCell) {
 
 function formatCurrency(n) {
   return Number(n).toFixed(2);
+}
+
+// short for `dedent` to de-indent template strings
+function d(s, ...args) {
+  if (Array.isArray(s))
+    return d(s.map((s1, i) => s1 + (args[i] ?? '')).join(''), ...args);
+  let nTrim = 0;
+  return s
+    .replace(/^[ \t]*\n/, '')
+    .split('\n')
+    .map((l, i) => {
+      const s1 = l.trimStart();
+      if (i) {
+        return l.length - s1.length <= nTrim ? s1 : l.slice(nTrim);
+      }
+      nTrim = l.length - s1.length;
+      return s1;
+    })
+    .join('\n');
 }
