a468a8d171bd4e5a98918b2d861c73df3aba2fbd
Satyam <satyam@satyam.com.ar>
2024-12-16 13:51:19 +0100

    Got all the processing of SaldoEnDivisa into the same file

--- Renta4/SaldoEnDivisa.mjs
+++ Renta4/SaldoEnDivisa.mjs
@@ -1,27 +1,59 @@
 import { readCSV, Fecha, sliceAfter, parseImporte } from './utils.mjs';
 
-export async function SaldoEnDivisa(file) {
+let saldoEnDivisa = [];
+
+export async function readSaldoEnDivisa(file) {
   console.log('Saldo En Divisa', file);
   const rows = await readCSV(file);
-  const movs = sliceAfter(rows, 'FECHA|CONCEPTO|MOVIMIENTOS|SALDO').map(
-    (row) => ({
+  saldoEnDivisa = saldoEnDivisa.concat(
+    sliceAfter(rows, 'FECHA|CONCEPTO|MOVIMIENTOS|SALDO').map((row) => ({
       fecha: row[0] ? Fecha.fromSabadell(row[0]) : null,
       concepto: row[1],
       importe: parseImporte(row[2]),
       saldo: parseImporte(row[3]),
-    })
+    }))
   );
-  // const firstRow = movs[0];
-  // if (firstRow.concepto === 'SALDO ANTERIOR') {
-  //   firstRow.fecha = new Fecha(movs[1].fecha.y, 1, 1);
-  //   firstRow.importe = 0;
-  // }
-  // const lastRow = movs.at(-1);
-  // if (lastRow.concepto === 'SALDO FINAL') {
-  //   lastRow.fecha = new Fecha(firstRow.fecha.y, 12, 31);
-  //   lastRow.importe = 0;
-  // }
-  return movs;
 }
 
-export default SaldoEnDivisa;
+export function processSaldoEnDivisa() {
+  let saldo = 0;
+  let lastFecha = '2000-01-01';
+  return saldoEnDivisa.reduce((acc, row) => {
+    switch (row.concepto) {
+      case 'SALDO ANTERIOR':
+        if (row.saldo) {
+          throw new Error(
+            `Saldo anterior inesperado cerca de ${lastFecha}: ${row.saldo}`
+          );
+        }
+        return acc;
+      case 'SALDO FINAL':
+        if (row.saldo !== saldo) {
+          throw new Error(
+            `Saldo final inesperado después de ${lastFecha}: ${row.saldo}`
+          );
+        }
+        return acc;
+      case 'ASIENTO DE APERTURA':
+        if (row.saldo !== saldo) {
+          throw new Error(
+            `Asiento de apertura inesperado después de ${lastFecha}: ${row.saldo}`
+          );
+        }
+        return acc;
+      default:
+        const f = row.fecha.toString();
+        if (lastFecha > f) {
+          throw new Error(`Fechas desordenadas ${lastFecha} - ${f}`);
+        }
+        lastFecha = f;
+        if (saldo + row.importe - row.saldo > 0.01) {
+          throw new Error(
+            `Saldo en ${row.concepto} de ${f} no da \nSaldo ant:${saldo}, importe: ${row.importe} no da ${row.saldo}`
+          );
+        }
+        saldo = row.saldo;
+        return [...acc, row];
+    }
+  }, []);
+}
