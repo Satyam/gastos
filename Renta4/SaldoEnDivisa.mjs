2014646c1bb9ca6c1f2196806c3d5d90c466c0d6
Satyam <satyam@satyam.com.ar>
2024-12-19 16:58:13 +0100

    En `concepto` de `SaldoEnDivisa` separar operación de destino.

--- Renta4/SaldoEnDivisa.mjs
+++ Renta4/SaldoEnDivisa.mjs
@@ -2,22 +2,53 @@ import { readCSV, Fecha, sliceAfter, parseImporte } from './utils.mjs';
 
 let saldoEnDivisa = [];
 
+const ops = [
+  'CAMPAÑA FONCUENTA',
+  'COMPRA DE',
+  'INTERESES',
+  'RETENCION A CUENTA',
+  'TRANSF. EMITIDA A',
+  'TRANSF. RECIBIDA DE',
+  'VENTA DE',
+  'SALDO ANTERIOR',
+  'SALDO FINAL',
+  'COMISION EMITIDA',
+  'ASIENTO DE APERTURA',
+];
+
 export async function readSaldoEnDivisa(file) {
   console.log('Saldo En Divisa', file);
   const rows = await readCSV(file);
   saldoEnDivisa = saldoEnDivisa.concat(
-    sliceAfter(rows, 'FECHA|CONCEPTO|MOVIMIENTOS|SALDO').map((row) => ({
-      fecha: row[0] ? Fecha.fromSabadell(row[0]) : null,
-      concepto: row[1],
-      importe: parseImporte(row[2]),
-      saldo: parseImporte(row[3]),
-    }))
+    sliceAfter(rows, 'FECHA|CONCEPTO|MOVIMIENTOS|SALDO').map((row) => {
+      const c = row[1];
+      let op = null,
+        fondo = '';
+      ops.forEach((o) => {
+        if (c.startsWith(o)) {
+          op = o;
+          fondo = c.replace(o, '').trim();
+        }
+      });
+      if (fondo.includes('BARREIRO')) fondo = 'Satyam';
+      if (fondo.includes('ROXANA')) fondo = 'Roxana';
+      if (!op) console.error(c);
+      return {
+        fecha: row[0] ? Fecha.fromSabadell(row[0]) : null,
+        concepto: c,
+        op,
+        fondo,
+        importe: parseImporte(row[2]),
+        saldo: parseImporte(row[3]),
+      };
+    })
   );
 }
 
 export function processSaldoEnDivisa() {
   let saldo = 0;
   let lastFecha = '2000-01-01';
+
   return saldoEnDivisa.reduce((acc, row) => {
     switch (row.concepto) {
       case 'SALDO ANTERIOR':
